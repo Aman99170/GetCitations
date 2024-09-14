ffc4af691437fa09edac7713101f18239c30a82f
Aman Kumar Singh <amansingh99170@gmail.com>
2024-09-15 01:49:43 +0530

    changed landing page now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,10 @@
 'use client'
-import { useSearchParams } from 'next/navigation';
 import { AuthProvider } from '../../context/AuthProvider';
 import { LandingPageResearcher } from '../../modules/LandingPageResearcher';
-import { LandingPageFreelancer } from '../../modules/LandingPageFreelancer';
 export default function Home() {
-
-
-  const searchParams = useSearchParams()
-  const userType = searchParams.get('userType')
-
   return (
     <AuthProvider>
-      {userType==="Freelancer" ? <LandingPageFreelancer/> :<LandingPageResearcher/> }
+      <LandingPageResearcher/>
     </AuthProvider>
   )
 }
