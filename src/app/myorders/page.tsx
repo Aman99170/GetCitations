79ac075d224599f014f9a887833ec3aba8819753
Aman Kumar Singh <amansingh99170@gmail.com>
2025-04-04 11:05:34 +0530

    made myorders and profile page private

--- src/app/myorders/page.tsx
+++ src/app/myorders/page.tsx
@@ -1,8 +1,20 @@
 'use client'
+import { useRouter } from "next/navigation";
 import { AuthProvider } from "../../../context/AuthProvider";
 import { MyOrders } from "../../../modules/myOrders";
+import { useAuthContext } from "../../../context/AuthContext";
+import { useEffect } from "react";
 
 export default function MyOrdersPage(){
+    const router = useRouter()
+    const{isLoggedIn} = useAuthContext()
+    useEffect(()=>{
+            if(!isLoggedIn){
+                router.push(`/`);
+            }else{
+                router.push("/profile")
+            }
+        },[isLoggedIn])
     return(
        <AuthProvider>
         <MyOrders/>
